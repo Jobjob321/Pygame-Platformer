2a9c4145e2249abec94dd598f3a52d7bee96afd3
Jobjob321 <100493024+Jobjob321@users.noreply.github.com>
2022-11-28 16:10:54 +0100

    removed performace

--- main.py
+++ main.py
@@ -67,10 +67,6 @@ while Running:
                 jump = False
 
     
-    if moveleft == True:
-        player.position.x -= 200 * dt
-    if moveright == True:
-        player.position.x += 200 * dt
     
     if player.position.y + 50 >= WINDOW_SIZE[1] and player.y_momentum > 0:
         player.y_momentum = 0
@@ -81,51 +77,55 @@ while Running:
 
 
 
-    collision_tolerance = 15
-    for object in objects:
-        if checkCollisions(object.x, object.y, object.width, object.height, player.position.x, player.position.y, 50, 50):
-            player_bottom = player.position.y  + 50
-            player_right = player.position.x + 50
-            object_bottom = object.y + object.height
-            object_right = object.x + object.width
-            if abs(object.y - player_bottom) < collision_tolerance:
-                if player.y_momentum >= 0:
-                    applyGravity = False
-                    player.y_momentum = 0
-                    CanJump = True
-            elif abs(object_bottom - player.position.y) < collision_tolerance:
-                if player.y_momentum < 0:
-                    player.y_momentum = 0
-                applyGravity = True
-            elif abs(object_right - player.position.x) < collision_tolerance:
-                if (xbefore - player.position.x) > 0:
-                    player.position.x = object_right
-            elif abs(object.x - player_right) < collision_tolerance:
-                if (xbefore - player.position.x) < 0:
-                    player.position.x = object.x - 50
-        object.draw(screen, scroll[0], scroll[1])
-    
-    for coin in coins:
-        if checkCollisions(coin.x, coin.y, coin.width, coin.height, player.position.x, player.position.y, 50, 50):
-            coinamount += 1
-            print(coinamount)
-            coins.remove(coin)
-        coin.draw(screen, scroll[0], scroll[1], coinimage)
+    collision_tolerance = 2
+    for i in range(10):
+        if moveleft == True:
+            player.position.x -= 25 * dt
+        if moveright == True:
+            player.position.x += 25 * dt
+        if CanJump == True and jump == True:
+            player.y_momentum = -70
+            CanJump = False
+        if applyGravity == True:
+            player.y_momentum += player.gravity * dt
+            CanJump = False
+        player.position.y += player.y_momentum * dt
+        for object in objects:
+            if checkCollisions(object.x, object.y, object.width, object.height, player.position.x, player.position.y, 50, 50):
+                player_bottom = player.position.y  + 50
+                player_right = player.position.x + 50
+                object_bottom = object.y + object.height
+                object_right = object.x + object.width
+                if abs(object.y - player_bottom) < collision_tolerance:
+                    if player.y_momentum >= 0:
+                        applyGravity = False
+                        player.y_momentum = 0
+                        CanJump = True
+                elif abs(object_bottom - player.position.y) < collision_tolerance:
+                    player.position.y = object_bottom
+                    if player.y_momentum <= 0:
+                        player.y_momentum = 0
+                    applyGravity = True
+                elif abs(object_right - player.position.x) < collision_tolerance:
+                    if (xbefore - player.position.x) > 0:
+                        player.position.x = object_right
+                elif abs(object.x - player_right) < collision_tolerance:
+                    if (xbefore - player.position.x) < 0:
+                        player.position.x = object.x - 50
+            object.draw(screen, scroll[0], scroll[1])
+        
+        for coin in coins:
+            if checkCollisions(coin.x, coin.y, coin.width, coin.height, player.position.x, player.position.y, 50, 50):
+                coinamount += 1
+                print(coinamount)
+                coins.remove(coin)
+            coin.draw(screen, scroll[0], scroll[1], coinimage)
 
-
-    if CanJump == True and jump == True:
-        player.y_momentum = -500
-        CanJump = False
-
-    if applyGravity == True:
-        player.y_momentum += player.gravity * dt
-        CanJump = False
 
 
 
     cointext = FONT.render("Coins: " + str(coinamount), 1, (0,0,0))
     screen.blit(cointext, (10, 10))
-    player.position.y += player.y_momentum * dt
     Player.Draw(screen, player.position.x - scroll[0], player.position.y - scroll[1])
     pygame.display.update()
     applyGravity = True
